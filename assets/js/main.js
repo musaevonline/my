6b173d8d4934cfdb436dee16dca4c57a402b8ead
Мусаев Амир Мусаевич <AMMusaev@sberbank.ru>
2022-05-03 18:47:04 +0300

    feat: gazer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,70 @@
 
 
 
-$(document).ready(function() {
+$(document).ready(function () {
 
     $('html').addClass('js-enabled');
 
     setup_nivo_lightbox();
     setup_dense();
 
-    $(window).load(function() {
-        $(".js-preloader").fadeOut(200, function() {
+    $(window).load(function () {
+        $(".js-preloader").fadeOut(200, function () {
             $(".js-main-container").fadeIn(30);
 
             setup_scrollreveal();
             setup_progress_bar_animation();
         });
-    });
+        const images = document.querySelectorAll('#gazer .gazer-sprite');
+        const gazer = document.querySelector('#gazer');
 
+        window.onmousemove = e => {
+            try {
+                if (gazer.style.display === 'none') {
+                    return
+                }
+
+                const rect = gazer.getBoundingClientRect();
+                const centerX = rect.left + rect.width / 2;
+                const centerY = rect.top + rect.height / 2;
+                const x = e.clientX - centerX
+                const y = e.clientY - centerY;
+                const cos = (0 * x + centerY * y) / (Math.sqrt(Math.pow(0, 2) + Math.pow(centerY, 2)) * Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)))
+                const angle = Math.acos(-cos) * 180 / Math.PI;
+                const fullAngle = x > 0 ? angle : 360 - angle;
+
+                const val = fullAngle / 360
+
+                const index = Math.floor(val * images.length)
+                const nextIndex = (index + 1) % images.length
+                const img = images[index]
+                const nextImg = images[nextIndex]
+                const part = 1 / images.length
+
+                if (img) {
+                    images.forEach(img => {
+                        img.style.opacity = 0;
+                        img.style.zIndex = '';
+                    })
+                    img.style.opacity = 1
+                    nextImg.style.zIndex = 1
+                    nextImg.style.opacity = (val - index * part) / part
+                }
+            } catch (e) { }
+        }
+    });
 });
 
-
-
-function setup_progress_bar_animation()
-{
+function setup_progress_bar_animation() {
     var $animation_elements = $("[class*='a-']");
     var $window = $(window);
 
-    $window.on('scroll resize', function() {
+    $window.on('scroll resize', function () {
         var window_height = $window.height();
         var window_top_position = $window.scrollTop();
         var window_bottom_position = (window_top_position + window_height);
 
-        $.each($animation_elements, function() {
+        $.each($animation_elements, function () {
             var $element = $(this);
             var element_height = $element.outerHeight();
             var element_top_position = $element.offset().top;
@@ -57,9 +90,8 @@ function setup_progress_bar_animation()
 
 
 
-function setup_dense()
-{
-    if($.isFunction($.fn.dense)) {
+function setup_dense() {
+    if ($.isFunction($.fn.dense)) {
 
         $('img').dense({
             'glue': '@'
@@ -70,9 +102,8 @@ function setup_dense()
 
 
 
-function setup_scrollreveal()
-{
-    if(typeof ScrollReveal !== 'undefined' && $.isFunction(ScrollReveal)) {
+function setup_scrollreveal() {
+    if (typeof ScrollReveal !== 'undefined' && $.isFunction(ScrollReveal)) {
 
         window.sr = ScrollReveal();
 
@@ -90,7 +121,7 @@ function setup_scrollreveal()
         var footer_config = $.extend(false, default_config, {
             duration: 1500,
             distance: 0,
-            viewOffset: {top: 0, right: 0, bottom: 100, left: 0}
+            viewOffset: { top: 0, right: 0, bottom: 100, left: 0 }
         });
 
         var default_delay = 30;
@@ -104,22 +135,20 @@ function setup_scrollreveal()
 
 
 
-function setup_nivo_lightbox()
-{
-    if($.isFunction($.fn.nivoLightbox))
-    {
+function setup_nivo_lightbox() {
+    if ($.isFunction($.fn.nivoLightbox)) {
         var $selector = $('.js-lightbox');
 
         // Hide all titles to prevent tooltip from showing
-        $selector.each(function() {
+        $selector.each(function () {
             var title = $(this).attr('title');
             $(this).attr('data-title', title);
             $(this).attr('title', '');
         });
 
         // On click, add titles back, so lightbox can display them
-        $selector.click(function() {
-            $selector.each(function() {
+        $selector.click(function () {
+            $selector.each(function () {
                 var title = $(this).attr('data-title');
                 $(this).attr('title', title);
             });
@@ -130,14 +159,14 @@ function setup_nivo_lightbox()
             theme: 'default',                             // The lightbox theme to use
             keyboardNav: true,                            // Enable/Disable keyboard navigation (left/right/escape)
             clickOverlayToClose: true,                    // If false clicking the "close" button will be the only way to close the lightbox
-            onInit: function(){},                         // Callback when lightbox has loaded
-            beforeShowLightbox: function(){},             // Callback before the lightbox is shown
-            afterShowLightbox: function(lightbox){},      // Callback after the lightbox is shown
-            beforeHideLightbox: function(){},             // Callback before the lightbox is hidden
+            onInit: function () { },                         // Callback when lightbox has loaded
+            beforeShowLightbox: function () { },             // Callback before the lightbox is shown
+            afterShowLightbox: function (lightbox) { },      // Callback after the lightbox is shown
+            beforeHideLightbox: function () { },             // Callback before the lightbox is hidden
             //afterHideLightbox: function(){},              // Callback after the lightbox is hidden
-            onPrev: function(element){},                  // Callback when the lightbox gallery goes to previous item
-            onNext: function(element){},                  // Callback when the lightbox gallery goes to next item
-            afterHideLightbox: function() {
+            onPrev: function (element) { },                  // Callback when the lightbox gallery goes to previous item
+            onNext: function (element) { },                  // Callback when the lightbox gallery goes to next item
+            afterHideLightbox: function () {
                 // Remove title to prevent tooltip from showing
                 $selector.attr('title', '');
             },
